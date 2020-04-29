498ad82985b441f812522180a30e471cef339e89
Samuel Razali <samuelrazali@gmail.com>
2020-04-29 19:30:44 +1000

    operation.js setCallbacks

--- src/operation.js
+++ src/operation.js
@@ -4,7 +4,7 @@ function getCurrentCity(callback) {
   setTimeout(function () {
 
     const city = "New York, NY";
-    callback(null, city);
+    callback(new Error ("fwef"), city);
 
   }, delayms)
 }
@@ -42,3 +42,61 @@ function getForecast(city, callback) {
 
   }, delayms)
 }
+
+
+suite.only("operations")
+
+
+function getCurrentCity(callback) {
+    setTimeout(function () {
+  
+      const city = "New York, NY";
+      callback(null, city);
+  
+    }, delayms)
+  }
+
+/*
+Create a function with both success and error callbacks
+and implement it on getCurrentCity
+*/
+
+function fetchCurrentCity(){
+
+    const operation = {}
+    
+        getCurrentCity(function(error, result){
+            if(error){
+                 operation.onError(error)
+                 return
+            }
+            operation.onSuccess(result)
+        })
+    
+    operation.setCallbacks = function setCallbacks(onSuccess, onError) {
+        operation.onSuccess = onSuccess;
+        operation.onError = onError;
+    }
+
+    return operation;
+ 
+}
+
+// Testing the fetchCurrentCity to see if it takes two callbacks
+// test("fetchCurrentCity with separate success and error callbacks", (done) => {
+//     fetchCurrentCity(
+//         result => done(),
+//         error => done(error)
+//     )
+
+// })
+
+test("fetchCurrentCity pass the callbacks later on", (done) => {
+
+    const operation = fetchCurrentCity()
+    operation.setCallbacks(
+        result => done(),
+        error => done(error)
+    )
+
+})
